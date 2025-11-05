36a13f3c2280dfd34ce9a93a153bfa59fd3dc7a6
Yang Ming <yahngming@gmail.com>
2025-11-05 20:57:49 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -7,13 +7,32 @@ let proxies = await produceArtifact({
   platform: "sing-box",
   produceType: "internal",
 });
-
 const existingTags = config.outbounds.map(o => o.tag);
 proxies = proxies.filter(p => !existingTags.includes(p.tag));
-config.outbounds.push(...proxies);
-
 const allTags = proxies.map(p => p.tag);
 const terminalTags = proxies.filter(p => !p.detour).map(p => p.tag);
+
+config.outbounds.push(...proxies);
+config.outbounds.map(i => {
+  if (['all'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+  if (['eco'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i))
+  }
+  if (['eu'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /🇫🇷|🇩🇪|🇮🇹|🇳🇱|🇪🇸|🇬🇧/i))
+  }
+  if (['hk'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /🇭🇰/i))
+  }
+  if (['jp'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /🇯🇵/i))
+  }
+  if (['us'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /🇺🇸/i))
+  }
+})
 config.outbounds.forEach(group => {
   if (!Array.isArray(group.outbounds) || group.tag === "direct") return;
   if (group.tag === "relay") {
