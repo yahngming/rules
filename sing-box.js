ac022dea82eacd3a005a6638c90aef9fde1f291b
Yang Ming <yahngming@gmail.com>
2025-11-05 21:34:18 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -1,5 +1,9 @@
 const { name, type } = $arguments;
 
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
+
 let config = JSON.parse($files[0]);
 let proxies = await produceArtifact({
   name,
@@ -7,47 +11,15 @@ let proxies = await produceArtifact({
   platform: "sing-box",
   produceType: "internal",
 });
-const existingTags = config.outbounds.map(o => o.tag);
-proxies = proxies.filter(p => !existingTags.includes(p.tag));
-const allTags = proxies.map(p => p.tag);
-const terminalTags = proxies.filter(p => !p.detour).map(p => p.tag);
-function getTags(proxies, regex) {
-  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
-}
 
 config.outbounds.push(...proxies);
 config.outbounds.map(i => {
-  if (['all'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies));
-  }
-  if (['eco'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));
-  }
-  if (['eu'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /🇫🇷|🇩🇪|🇮🇹|🇳🇱|🇪🇸|🇬🇧/i));
-  }
-  if (['hk'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /🇭🇰/i));
-  }
-  if (['jp'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /🇯🇵/i));
-  }
-  if (['us'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /🇺🇸/i));
-  }
-});
-config.outbounds.forEach(group => {
-  if (!Array.isArray(group.outbounds) || group.tag === "direct") return;
-  if (group.tag === "relay") {
-    group.outbounds.push(...terminalTags);
-  } else {
-    group.outbounds.push(...allTags);
-  }
-});
-config.outbounds.forEach(group => {
-  if (Array.isArray(group.outbounds)) {
-    group.outbounds = [...new Set(group.outbounds)];
-  }
+  if (['all'].includes(i.tag)) {i.outbounds.push(...getTags(proxies));}
+  if (['eco'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
+  if (['eu'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇫🇷|🇩🇪|🇮🇹|🇳🇱|🇪🇸|🇬🇧/i));}
+  if (['hk'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇭🇰/i));}
+  if (['jp'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇯🇵/i));}
+  if (['us'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇺🇸/i));}
 });
 
 $content = JSON.stringify(config, null, 2);
