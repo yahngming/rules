48964577686a01e38495bf79cfb07b63310d65f5
Yang Ming <yahngming@gmail.com>
2025-12-13 21:42:13 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -15,16 +15,11 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies);
 config.outbounds.map(i => {
   if (['default'].includes(i.tag)) {i.outbounds.push(...getTags(proxies));}
-  if (['white'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
-  if (['emby'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
-  if (['github'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
-  if (['netflix'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
-  if (['steam'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
-  if (['youtube'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|直连|TEST/i));}
-  if (['eu'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇫🇷|🇩🇪|🇮🇹|🇳🇱|🇪🇸|🇬🇧/i));}
-  if (['hk'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇭🇰/i));}
-  if (['jp'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇯🇵/i));}
-  if (['us'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇺🇸/i));}
+  if (['white'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建/i));}
+  if (['emby'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /Emby|自建|实验|直连|TEST/i));}
+  if (['netflix'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇦🇷|🇵🇰|🇹🇷|自建|实验|直连|TEST/i));}
+  if (['steam'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇦🇷|🇭🇰|🇯🇵|🇵🇰|🇸🇬|🇹🇼|🇹🇷/i));}
+  if (['youtube'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇺🇸/i));}
 });
 
 $content = JSON.stringify(config, null, 2);
