997a433668ce998d47de866cbb2ca12f9ca28c4d
Yang Ming <yahngming@gmail.com>
2026-06-28 20:27:22 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -1,24 +1,24 @@
 const { name, type } = $arguments;
 
 function getTags(proxies, regex) {
-  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+	return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
 }
 
 let config = JSON.parse($files[0]);
 let proxies = await produceArtifact({
-  name,
-  type: /^1$|col/i.test(type) ? "collection" : "subscription",
-  platform: "sing-box",
-  produceType: "internal",
+	name,
+	type: /^1$|col/i.test(type) ? "collection" : "subscription",
+	platform: "sing-box",
+	produceType: "internal",
 });
 
 config.outbounds.push(...proxies);
 config.outbounds.map(i => {
-  if (['force'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建/i));}
-  if (['game'].includes(i.tag)) {i.outbounds.push(...getTags(proxies));}
-  if (['stream'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|日用|0\./i));}
-  if (['us'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇺🇸/i));}
-  if (['default'].includes(i.tag)) {i.outbounds.push(...getTags(proxies));}
+	if (['force'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建/i));}
+	if (['game'].includes(i.tag)) {i.outbounds.push(...getTags(proxies));}
+	if (['stream'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /自建|实验|日用|0\./i));}
+	if (['us'].includes(i.tag)) {i.outbounds.push(...getTags(proxies, /🇺🇸/i));}
+	if (['default'].includes(i.tag)) {i.outbounds.push(...getTags(proxies));}
 });
 
 $content = JSON.stringify(config, null, 2);
