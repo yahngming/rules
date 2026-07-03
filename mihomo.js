f24a53e29d3e1ce4289ba60d91ccd10679ce77ee
Yang Ming <yahngming@gmail.com>
2026-07-04 03:55:04 +0800

    Update mihomo.js

--- mihomo.js
+++ mihomo.js
@@ -4,14 +4,24 @@ function getNames(proxies, regex) {
 
 let baseContent = $content ?? (typeof $files !== 'undefined' ? $files[0] : "");
 let config = ProxyUtils.yaml.safeLoad(baseContent);
-let proxies = config.proxies.filter(p => p.type && p.type !== 'select');
+let dynamicProxies = config.proxies || [];
+config.proxies = dynamicProxies.map(p => {
+	let cleaned = {};
+	for (let key in p) {
+		if (!key.startsWith('_')) {
+			cleaned[key] = p[key];
+		}
+	}
+	return cleaned;
+});
+let cleanProxies = config.proxies.filter(p => p.type && p.type !== 'select');
 
 config['proxy-groups'] = (config['proxy-groups'] || []).map(group => {
-	if (['DEFAULT'].includes(group.name)) {group.proxies.push(...getNames(proxies));}
-	if (['FORCE'].includes(group.name)) {group.proxies.push(...getNames(proxies, /自建/i));}
-	if (['GAME'].includes(group.name)) {group.proxies.push(...getNames(proxies));}
-	if (['STREAM'].includes(group.name)) {group.proxies.push(...getNames(proxies, /自建|实验|日用|0\./i));}
-	if (['US'].includes(group.name)) {group.proxies.push(...getNames(proxies, /🇺🇸/i));}
+	if (['DEFAULT'].includes(group.name)) {group.proxies.push(...getNames(cleanProxies));}
+	if (['FORCE'].includes(group.name)) {group.proxies.push(...getNames(cleanProxies, /自建/i));}
+	if (['GAME'].includes(group.name)) {group.proxies.push(...getNames(cleanProxies));}
+	if (['STREAM'].includes(group.name)) {group.proxies.push(...getNames(cleanProxies, /自建|实验|日用|0\./i));}
+	if (['US'].includes(group.name)) {group.proxies.push(...getNames(cleanProxies, /🇺🇸/i));}
 	return group;
 });
 
